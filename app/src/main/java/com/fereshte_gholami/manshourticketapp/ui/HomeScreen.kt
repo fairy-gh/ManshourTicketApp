d484b5a4bc2cb3b941d6e9f422a941515259fb9e
fereshte gholami <f.gholami.dev@gmail.com>
2023-11-11 16:35:56 +0330

    update HomeScreenSeats.kt

--- app/src/main/java/com/fereshte_gholami/manshourticketapp/ui/HomeScreen.kt
+++ app/src/main/java/com/fereshte_gholami/manshourticketapp/ui/HomeScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
@@ -15,7 +14,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color.Companion.Black
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
 import com.fereshte_gholami.manshourticketapp.data.model.HomeUI
 import com.fereshte_gholami.manshourticketapp.ui.composables.HomeScreenHeader
 import com.fereshte_gholami.manshourticketapp.ui.composables.HomeScreenSeats
@@ -52,23 +50,20 @@ fun HomeScreenContent(
             .fillMaxSize()
             .background(Black)
     ) {
-           Column{
-               HomeScreenHeader(
-                   modifier = Modifier.fillMaxWidth(),
-                   onCloseClicked = onCloseClicked
-               )
-               HomeScreenSeats(
-                   modifier = Modifier
-                       .fillMaxWidth()
-                       .padding(top = 250.dp),
-                   seatsPairs = homeUI.seatStateList
-                       .chunked(2)
-                       .map {
-                           it.first() to it.last()
-                       },
-                   onSeatClicked = onSeatClicked
-               )
-           }
+        Column {
+            HomeScreenHeader(
+                modifier = Modifier.fillMaxWidth(),
+                onCloseClicked = onCloseClicked
+            )
+            HomeScreenSeats(
+                seatsPairs = homeUI.seatStateList
+                    .chunked(2)
+                    .map {
+                        it.first() to it.last()
+                    },
+                onSeatClicked = onSeatClicked
+            )
+        }
     }
 }
 
--- app/src/main/java/com/fereshte_gholami/manshourticketapp/ui/composables/HomeScreenSeats.kt
+++ app/src/main/java/com/fereshte_gholami/manshourticketapp/ui/composables/HomeScreenSeats.kt
@@ -1,13 +1,13 @@
 package com.fereshte_gholami.manshourticketapp.ui.composables
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import com.fereshte_gholami.manshourticketapp.data.model.SeatState
 
 @Composable
-fun HomeScreenSeats(modifier: Modifier,
-                    seatsPairs: List<Pair<SeatState, SeatState>>,
-                    onSeatClicked: (id: Int) -> Unit){
+fun HomeScreenSeats(
+    seatsPairs: List<Pair<SeatState, SeatState>>,
+    onSeatClicked: (id: Int) -> Unit
+) {
     SeatLayout(seatPair = seatsPairs, onSeatClicked = onSeatClicked)
     HomeScreenSeatAvailabilitySection()
 }
